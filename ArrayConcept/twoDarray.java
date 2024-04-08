78848345301cd47d3694d2baefd025c628c5cc03
valeriansword <chandrakumarxii@gmail.com>
2024-04-08 23:05:26 +0530

    added

---
+++ ArrayConcept/twoDarray.java
@@ -0,0 +1,15 @@
+package ArrayConcept;
+// import java.util.Scanner;
+public class twoDarray {
+    public static void main(String[] args) {
+        // Scanner in=new Scanner(System.in);
+        int[][] a={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        System.out.println(a.length);
+
+
+    }
+}
